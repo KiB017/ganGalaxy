57859e4878e6040d8fd52ec6155e9076485110ec
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2024-04-21 00:01:50 -0400

    Second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img 
+        src={require('./images/ganGalaxy_logo.webp')}
+        className='ganGalaxy-logo'
+      />
+    </div>
+  );
+}
+
+export default App;
