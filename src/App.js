8d90b63c96d553ae07e66a49668c43f978e4c58e
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2024-04-22 16:38:54 -0400

    creating the components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import ganGalaxyEntrance from './images/ganGalaxy-entrance.webp';
+import ganGalaxyLogo from './images/ganGalaxy_logo.webp';
 
 function App() {
   return (
     <div className="App">
-      <img 
-        src={require('./images/ganGalaxy_logo.webp')}
-        className='ganGalaxy-logo'
-      />
+      <header>
+        <img 
+          className='ganGalaxy-logo'
+          src={ganGalaxyLogo} 
+          alt=''
+        />
+        <p className='header-text'>The universe in your hands</p>
+      </header>
+
+      <div className = "welcome-container">
+        <img
+          src={ganGalaxyEntrance}
+          className = "welcome-container-entrance"
+          alt = "ganGalaxy Museam entrance"
+        />
+        <p className="welcome-text">Welcome to the world of astronomy</p>
+      </div>
+      
     </div>
   );
 }
