1751043bf2770da609768b7a3c9a1512ed7521af
Kiyoshi Marzo <marzokiyoshi@gmail.com>
2024-04-22 18:17:45 -0400

    adjusting the header

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <div className='App'>
+      <div className = 'header-container'>
         <img 
-          className='ganGalaxy-logo'
-          src={require('./images/ganGalaxy_logo.webp')} 
-          alt=''
+          className = 'ganGalaxy-logo'
+          src = {require('./images/ganGalaxy_logo.webp')} 
+          alt = ''
         />
-        <p className='header-text'>The universe in your hands</p>
-      </header>
+        <br/>
+        <p className = 'header-text'>The universe in your hands</p>
+      </div>
 
       <div className = "welcome-container">
         <img 
           className = 'welcome-background'
           src = {require('./images/galaxy-background.webp')}
+          alt = ''
         />
         <img
-          src={require('./images/ganGalaxy-entrance.webp')}
-          className = "welcome-container-entrance"
-          alt = "ganGalaxy Museam entrance"
+          src = {require('./images/ganGalaxy-entrance.webp')}
+          className = 'welcome-container-entrance'
+          alt = 'ganGalaxy Museam entrance'
         />
         <p className="welcome-text">Welcome to the world of astronomy</p>
       </div>
